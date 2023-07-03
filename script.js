6583772d8a714900cee1aa959827a410bc65cdef
FawazCodes <108174119+FawazCodes@users.noreply.github.com>
2023-07-03 23:57:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function getRandomColor() {
   return randomColor;
 }
 
-const guessInput = document.getElementById("guess");
+const numberDisplay = document.getElementById("number-display");
 const submitBtn = document.getElementById("submit-btn");
 const resetBtn = document.getElementById("reset-btn");
 const message = document.getElementById("message");
@@ -45,82 +45,74 @@ function updateLanguage() {
 
 updateLanguage();
 
-let hiddenNumber = generateHiddenNumber();
+const hiddenNumber = generateHiddenNumber();
 let attempts = 5;
+let computerGuess = generateRandomGuess();
 
-guessInput.addEventListener("input", () => {
-  submitBtn.disabled = !isValidGuess(guessInput.value);
-});
+numberDisplay.textContent = "****";
 
 submitBtn.addEventListener("click", () => {
-  const guess = guessInput.value;
-  const feedback = compareGuess(hiddenNumber, guess);
+  const feedback = compareGuess(hiddenNumber, computerGuess);
 
   if (feedback === "+4") {
-    message.textContent = "Congratulations! You've won the game!";
+    message.textContent = "Oh no! The computer has guessed your number!";
     submitBtn.disabled = true;
     resetBtn.hidden = false;
   } else {
     attempts--;
 
     if (attempts === 0) {
-      message.textContent = `Game over! The hidden number was ${hiddenNumber}.`;
+      message.textContent = "Congratulations! The computer could not guess your number!";
       submitBtn.disabled = true;
       resetBtn.hidden = false;
     } else {
-      message.textContent = `Remaining attempts: ${attempts}.`;
+      message.textContent = `The computer has ${feedback} correct digits.`;
+      computerGuess = generateRandomGuess();
     }
 
-    addToGuessesGrid(guess, feedback);
+    addToGuessesGrid(computerGuess, feedback);
   }
-
-  guessInput.value = "";
-  submitBtn.disabled = true;
 });
 
 resetBtn.addEventListener("click", () => {
-  hiddenNumber = generateHiddenNumber();
   attempts = 5;
+  computerGuess = generateRandomGuess();
   message.textContent = "";
   guessesGrid.innerHTML = "";
   resetBtn.hidden = true;
-  submitBtn.disabled = true;
+  submitBtn.disabled = false;
 });
 
 function generateHiddenNumber() {
-  let number = "";
-  while (number.length < 4) {
-    const randomDigit = Math.floor(Math.random() * 10);
-    if (!number.includes(randomDigit)) {
-      number += randomDigit;
-    }
-  }
-  return number;
+  const hiddenNumber = prompt("Enter a four-digit number for the computer to guess:");
+  return hiddenNumber;
 }
 
-function isValidGuess(guess) {
-  return guess.length === 4 && new Set(guess).size === 4 && /^\d+$/.test(guess);
+function generateRandomGuess() {
+  let randomGuess = "";
+  while (randomGuess.length < 4) {
+    const randomDigit = Math.floor(Math.random() * 10);
+    if (!randomGuess.includes(randomDigit)) {
+      randomGuess += randomDigit;
+    }
+  }
+  return randomGuess;
 }
 
 function compareGuess(hiddenNumber, guess) {
   let correctDigits = 0;
-  let misplacedDigits = 0;
 
   for (let i = 0; i < 4; i++) {
     if (hiddenNumber[i] === guess[i]) {
       correctDigits++;
-    } else if (hiddenNumber.includes(guess[i])) {
-      misplacedDigits++;
     }
   }
 
   let feedback = "";
   if (correctDigits > 0) {
     feedback += `+${correctDigits}`;
-  }
-  if (misplacedDigits > 0) {
-    if (feedback) feedback += ", ";
-    feedback += `-${misplacedDigits}`;
+  } else {
+    feedback = "0";
   }
 
   return feedback;
